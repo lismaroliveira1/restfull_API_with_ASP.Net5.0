cc252ed37309339e3a95989b95bfbd5592cb9652
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-22 23:50:07 -0300

    create project

---
+++ restfull_API_with_ASP.Net5.0/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace restfull_API_with_ASP.Net5._0
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
