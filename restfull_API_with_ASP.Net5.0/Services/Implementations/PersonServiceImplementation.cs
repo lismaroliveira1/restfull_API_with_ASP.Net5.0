b9e73a371f544ac817e86b3f5a19839add146d94
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-26 11:49:52 -0300

    add IPersonServiceImplementations

---
+++ restfull_API_with_ASP.Net5.0/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using restfull_API_with_ASP.Net5._0.model;
+
+namespace restfull_API_with_ASP.Net5._0.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+
+    {
+        private volatile int count;
+        public Person Create(Person person) {
+            return person;
+        }
+       public Person FindByID(long id) {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FistName = "Lismar",
+                LastName = "Oliveira",
+                AddressName = "Salvador, Bahia - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FistName = "Person Name" + i,
+                LastName = "Person Last Name" + i,
+                AddressName = "Some Address" + i,
+                Gender = "Some Name" + i
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person Update(Person person) {
+            return person;
+        }
+        void Delete(long id) { }
+
+        List<Person> IPersonService.FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        void IPersonService.Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+    
+
