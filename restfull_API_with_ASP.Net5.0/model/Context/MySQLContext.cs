fb4d63d2c53f8a73bcdfcf9a764592121aa8c140
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-27 19:19:29 -0300

    add MySQLContext

---
+++ restfull_API_with_ASP.Net5.0/model/Context/MySQLContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace restfull_API_with_ASP.Net5._0.model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+            
+    }
+}
