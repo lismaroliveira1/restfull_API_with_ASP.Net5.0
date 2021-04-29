304c007db3f98ffb59d0d0c7642be9f38ba747f2
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-29 19:22:44 -0300

    change Update metthod to return null whwen person doesn`t exists

--- restfull_API_with_ASP.Net5.0/Repository/Implementations/PersonRepositoryImplementation.cs
+++ restfull_API_with_ASP.Net5.0/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -40,7 +40,7 @@ namespace restfull_API_with_ASP.Net5._0.Repositor.Implementations
         }
 
         public Person Update(Person person) {
-            if (!Exists(person.Id)) return new Person();
+            if (!Exists(person.Id)) return null;
             var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
             if(result != null)
             {
